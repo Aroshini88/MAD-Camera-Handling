9a491522eb51f8cbfe1c42c315162ed865d47b47
Aroshini88 <arodeez88@gmail.com>
2021-08-21 13:13:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/imagehandling/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.imagehandling;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageview = (ImageView) this.findViewById(R.id.imageview1);
+        Button photobutton = (Button) this.findViewById(R.id.button1);
+
+        photobutton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(photo);
+        }
+    }
+    }
+
+
